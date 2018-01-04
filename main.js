a07fca53b5dfe320ad55afbbf01d011966aec4d6
Atsuki Demizu <demmys.atsuki@gmail.com>
2018-01-04 18:59:56 +0900

    Added ip address posting feature

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const firebase = require('firebase');
 const config = require('./config.js');
 const controller = require('./controller.js');
@@ -5,6 +6,29 @@ const controller = require('./controller.js');
 firebase.initializeApp(config);
 const db = firebase.database();
 
+let ip = getLocalIpv4();
+db.ref('/raspberry').set({ ip: ip });
+
 controller.register(db, '/ps4', 'command', require('./ps4_actions.js'));
 
-console.log('Ms.Raspberry is now ready.\n');
+console.log('Ms.Raspberry is now ready on ' + ip + '.');
+
+function getLocalIpv4() {
+    let interfaces = os.networkInterfaces();
+    for (let name in interfaces) {
+        if (name === 'lo') {
+            continue;
+        }
+        let ip = null;
+        interfaces[name].some(address => {
+            if (address.family === 'IPv4') {
+                ip = address.address;
+                return true;
+            }
+        });
+        if (ip !== null) {
+            return ip;
+        }
+    }
+    return '';
+}
